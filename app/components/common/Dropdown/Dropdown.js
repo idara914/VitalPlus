728f3be1cf9c96db5af51ea35329655f2fa24b8f
AmmarMasood <ammar.masood98@gmail.com>
2024-08-13 22:38:58 +0500

    fixed a build issue

--- app/components/common/Dropdown/Dropdown.js
+++ app/components/common/Dropdown/Dropdown.js
@@ -1,51 +1,53 @@
-import { useState } from "react";
-import Link from "next/link";
-import styles from "./Dropdown.module.css";
+// import { useState } from "react";
+// import Link from "next/link";
+// import styles from "./Dropdown.module.css";
 
-const Dropdown = ({ children }) => {
-  return ({ title, options, toggleMobileMenu, ...props }) => {
-    const [isOpen, setIsOpen] = useState(false);
+// const Dropdown = ({ children }) => {
+//   return ({ title, options, toggleMobileMenu, ...props }) => {
+//     const [isOpen, setIsOpen] = useState(false);
 
-    const handleMouseEnter = () => {
-      setIsOpen(true);
-    };
+//     const handleMouseEnter = () => {
+//       setIsOpen(true);
+//     };
 
-    const handleMouseLeave = () => {
-      setIsOpen(false);
-    };
+//     const handleMouseLeave = () => {
+//       setIsOpen(false);
+//     };
 
-    return (
-      <div
-        className={styles.dropdown}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className={styles.dropdownButton}
-        >
-          {title}
-        </button>
-        {isOpen && (
-          <div className={styles.dropdownContent}>
-            {options.map((option, index) => (
-              <Link
-                key={index}
-                href={option.href}
-                onClick={() => {
-                  toggleMobileMenu();
-                  setIsOpen(false);
-                }}
-              >
-                {option.label}
-              </Link>
-            ))}
-          </div>
-        )}
-        <children {...props} />
-      </div>
-    );
-  };
-};
+//     return (
+//       <div
+//         className={styles.dropdown}
+//         onMouseEnter={handleMouseEnter}
+//         onMouseLeave={handleMouseLeave}
+//       >
+//         <button
+//           onClick={() => setIsOpen(!isOpen)}
+//           className={styles.dropdownButton}
+//         >
+//           {title}
+//         </button>
+//         {isOpen && (
+//           <div className={styles.dropdownContent}>
+//             {options.map((option, index) => (
+//               <Link
+//                 key={index}
+//                 href={option.href}
+//                 onClick={() => {
+//                   toggleMobileMenu();
+//                   setIsOpen(false);
+//                 }}
+//               >
+//                 {option.label}
+//               </Link>
+//             ))}
+//           </div>
+//         )}
+//         <children {...props} />
+//       </div>
+//     );
+//   };
+// };
 
-export default withDropdown;
+// export default withDropdown;
+
+export default function DropDown() {}
