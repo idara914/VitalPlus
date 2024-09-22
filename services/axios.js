de7fa09d944b915d9ef2d0249d3d68542be673f0
dany-dev <daniyalhussainsiddiqui1996@gmail.com>
2024-09-23 00:40:02 +0500

    Axios Link updated

--- services/axios.js
+++ services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://129.146.244.77:3000/api',
+  baseURL: 'https://vital-plus.xyz:3000/api',
 });
 
 export default instance;
