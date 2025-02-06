80463a67a0c62f41c09b8b8a93a22156d64c3056
idara914 <idara914@gmail.com>
2025-02-05 19:51:43 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-import pg from 'pg'
-import dotenv from 'dotenv';  
+import pg from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const { Pool } = pg
+const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false, // Enables SSL for production
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
-})
+});
 
 export default pool;
