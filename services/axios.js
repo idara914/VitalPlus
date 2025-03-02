d8c0e89e09c3b79b23592a08d8eb4c1bfe8027bb
idara914 <idara914@gmail.com>
2025-03-02 13:48:52 -0600

    Update axios.js

--- services/axios.js
+++ services/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://vital-plus.xyz:3000/api',
+  baseURL: "https://vital-plus.xyz:3000", // ✅ Removed extra `/api`
 });
 
 export default instance;
