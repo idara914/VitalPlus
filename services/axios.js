c470d1b601809567dde91dc988f858524e58f0c1
idara914 <idara914@gmail.com>
2025-03-30 03:12:31 -0700

    Update axios.js

--- services/axios.js
+++ services/axios.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://vital-plus.xyz", // ✅ Remove `:3000`
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://www.vital-plus.xyz",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
+  withCredentials: true, // Optional: If you need cookies/session
 });
 
 export default instance;
