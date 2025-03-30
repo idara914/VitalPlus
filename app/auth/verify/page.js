aa19082b30093ecf2699b312be88bfe93290d997
idara914 <idara914@gmail.com>
2025-03-30 03:44:13 -0700

    Update page.js

--- app/auth/verify/page.js
+++ app/auth/verify/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState } from "react";
 import TextField from "../../components/common/TextField/TextField";
 import Button from "../../components/common/Button/Button";
@@ -120,4 +122,5 @@ export default function Verify() {
     </AuthLayout>
   );
 }
+
 
