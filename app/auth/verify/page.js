e7571d56fee5f5e5f3dc29d380e92b1ac7f0a353
idara914 <idara914@gmail.com>
2025-04-02 22:46:15 -0700

    Update page.js

--- app/auth/verify/page.js
+++ app/auth/verify/page.js
@@ -1,14 +1,10 @@
 export const dynamic = "force-dynamic";
-
 import VerifyForm from "./VerifyForm";
 import AuthLayout from "@/app/components/layouts/AuthLayout";
 
 export default function VerifyPage() {
   return (
-    <AuthLayout
-      heading="Verify your email"
-      text="Please enter the OTP sent to your email."
-    >
+    <AuthLayout heading="Verify your email" text="Please enter the OTP sent to your email.">
       <VerifyForm />
     </AuthLayout>
   );
