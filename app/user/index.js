a5f3eb08dc715388483148e82ae80f0377b8d642
idara914 <idara914@gmail.com>
2025-04-04 14:58:28 -0700

    Update index.js

--- app/user/index.js
+++ app/user/index.js
@@ -16,5 +16,19 @@ export const signIn = async (token, user) => {
       const payload = JSON.parse(atob(token.split(".")[1]));
       localStorage.setItem("userId", payload.id);
       localStorage.setItem("email", payload.email);
+    } catch (error) {
+      console.error("Failed to decode token", error);
     }
+  }
+  */
+};
 
+export const signOut = async () => {
+  await removeSession();
+
+  if (typeof window !== "undefined") {
+    document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    localStorage.removeItem("userId");
+    localStorage.removeItem("email");
+  }
+};
