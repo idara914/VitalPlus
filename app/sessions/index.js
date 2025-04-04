8ef150e43860e2f19a6ebe913895a635ecdfb20a
idara914 <idara914@gmail.com>
2025-04-03 17:05:40 -0700

    Update index.js

--- app/sessions/index.js
+++ app/sessions/index.js
@@ -1,23 +1,13 @@
 'use server';
 
-import { cookies } from 'next/headers';
+import { setSession, removeSession } from './../sessions';
+import jwt_decode from 'jwt-decode';
 
-export const getSession = async () => {
-    const cookieStore = cookies();
-    const session = { token: cookieStore.get('token')?.value, user: JSON.parse(cookieStore.get('user')?.value) };
-    return session;
+export const signIn = async (token) => {
+  const user = jwt_decode(token); // Extracts user { id, email, ... }
+  await setSession(token, user);  // Save user and token in session
 };
 
-export const setSession = async (token, user) => {
-    const cookieStore = cookies();
-    cookieStore.set('token', token);
-    cookieStore.set('user', JSON.stringify(user));
-};
-
-export const removeSession = async () => {
-    const cookieStore = cookies();
-    cookieStore.delete('token');
-    cookieStore.delete('user');
-
-    return true;
+export const signOut = async () => {
+  await removeSession();
 };
