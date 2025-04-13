90b4de964c41ec434ed075e98ae15af1ec7b7a37
idara914 <idara914@gmail.com>
2025-04-13 11:08:24 -0700

    Update AuthLayout.js

--- app/components/layouts/AuthLayout.js
+++ app/components/layouts/AuthLayout.js
@@ -1,6 +1,9 @@
+"use client";
+
 import styles from "../../assets/auth.module.css";
 import LogoWhite from "../../../public/logos/logo-white.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function AuthLayout({ heading, text, children }) {
   return (
@@ -8,7 +11,13 @@ export default function AuthLayout({ heading, text, children }) {
       <div className={styles.containerLeft}>
         <div className={styles.containerLeftInner}>
           <h1 className={styles.heading}>{heading}</h1>
-          <Image src={LogoWhite} alt="vitalplus" />
+          <Link href="https://www.vital-plus.xyz/">
+            <Image
+              src={LogoWhite}
+              alt="vitalplus"
+              style={{ cursor: "pointer" }}
+            />
+          </Link>
           <p className={styles.paragraph}>{text}</p>
         </div>
       </div>
