ca108a47c780a229ad1ce055c962f3700f36c051
idara914 <idara914@gmail.com>
2025-04-18 17:07:29 -0700

    Update Demographics.js

--- app/member/new/components/Demographics.js
+++ app/member/new/components/Demographics.js
@@ -62,57 +62,7 @@ function DemographicForm({ onClick }) {
           }}
         />
       </Form.Item>
-      <h1 className={styles.heading}>Insurance Payor</h1>
-      <div className={styles.gridContainer}>
-        <Form.Item label="Payor Name" style={customStyle} name="payorName">
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="COB Policy" name="cob">
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="Career" name="career" style={customStyle}>
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="Payor Responsibility" name="payorResponsibility">
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="Address" name="address" style={customStyle}>
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="State" name="state">
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="City" name="city" style={customStyle}>
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="Zip Code" name="zipCode">
-          <Input placeholder="Enter here" />
-        </Form.Item>
 
-        <Form.Item label="EFF Date" name="effDate" style={customStyle}>
-          <DatePicker
-            placeholder="Enter here"
-            style={{
-              width: "100%",
-              borderRadius: "8px",
-              padding: "8px",
-            }}
-          />
-        </Form.Item>
-        <Form.Item label="Term Date" name="termDate">
-          <DatePicker
-            placeholder="Enter here"
-            style={{
-              width: "100%",
-              borderRadius: "8px",
-              padding: "8px",
-            }}
-          />
-        </Form.Item>
-      </div>
-      <Form.Item label="Phone" name="phone">
-        <Input placeholder="Enter here" />
-      </Form.Item>
       <h1 className={styles.heading}>Patient preference</h1>
       <Form.Item
         label=""
@@ -139,3 +89,4 @@ function DemographicForm({ onClick }) {
 }
 
 export default DemographicForm;
+
