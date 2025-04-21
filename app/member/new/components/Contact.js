92f0bb4b651e6f2f6dc286b870783f22163662ac
idara914 <idara914@gmail.com>
2025-04-21 11:10:44 -0700

    Update Contact.js

--- app/member/new/components/Contact.js
+++ app/member/new/components/Contact.js
@@ -1,6 +1,7 @@
+"use client";
+
 import React from "react";
 import { Form, Input } from "antd";
-import { useFormInstance } from "antd/es/form/context";
 import axios from "axios";
 import styles from "./Contact.module.css";
 import Button from "@/app/components/common/Button/Button";
@@ -10,7 +11,7 @@ const customStyle = {
 };
 
 function Contact() {
-  const form = useFormInstance();
+  const [form] = Form.useForm();
 
   const handleSave = async () => {
     const values = form.getFieldsValue(true);
@@ -24,65 +25,64 @@ function Contact() {
   };
 
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.gridContainer}>
-        <Form.Item
-          label="Emergency Contact Name"
-          style={customStyle}
-          name="emergencyContactName"
-        >
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="Relationship" name="relationship">
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="Contact" name="contact" style={customStyle}>
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="Address" name="address">
-          <Input placeholder="Enter here" />
-        </Form.Item>
+    <Form form={form}>
+      <div className={styles.mainContainer}>
+        <div className={styles.gridContainer}>
+          <Form.Item
+            label="Emergency Contact Name"
+            style={customStyle}
+            name="emergencyContactName"
+          >
+            <Input placeholder="Enter here" />
+          </Form.Item>
+          <Form.Item label="Relationship" name="relationship">
+            <Input placeholder="Enter here" />
+          </Form.Item>
+          <Form.Item label="Contact" name="contact" style={customStyle}>
+            <Input placeholder="Enter here" />
+          </Form.Item>
+          <Form.Item label="Address" name="address">
+            <Input placeholder="Enter here" />
+          </Form.Item>
 
-        <Form.Item
-          label="Secondary Physician"
-          name="secondaryPhysician"
-          style={customStyle}
-        >
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item
-          label="Secondary Physician Number"
-          name="secondaryPhysicianNumber"
-        >
-          <Input placeholder="Enter here" />
-        </Form.Item>
+          <Form.Item
+            label="Secondary Physician"
+            name="secondaryPhysician"
+            style={customStyle}
+          >
+            <Input placeholder="Enter here" />
+          </Form.Item>
+          <Form.Item
+            label="Secondary Physician Number"
+            name="secondaryPhysicianNumber"
+          >
+            <Input placeholder="Enter here" />
+          </Form.Item>
 
-        <Form.Item label="State" name="state" style={customStyle}>
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="City" name="city">
-          <Input placeholder="Enter here" />
-        </Form.Item>
+          <Form.Item label="State" name="state" style={customStyle}>
+            <Input placeholder="Enter here" />
+          </Form.Item>
+          <Form.Item label="City" name="city">
+            <Input placeholder="Enter here" />
+          </Form.Item>
 
-        <Form.Item label="Zip Code" name="zip" style={customStyle}>
-          <Input placeholder="Enter here" />
-        </Form.Item>
-        <Form.Item label="Address" name="address">
-          <Input placeholder="Enter here" />
+          <Form.Item label="Zip Code" name="zip" style={customStyle}>
+            <Input placeholder="Enter here" />
+          </Form.Item>
+        </div>
+
+        <Form.Item>
+          <Button
+            htmltype="submit"
+            onClick={handleSave}
+            text={"Save"}
+            customStyle={{
+              width: "100%",
+            }}
+          />
         </Form.Item>
       </div>
-
-      <Form.Item>
-        <Button
-          htmltype="submit"
-          onClick={handleSave}
-          text={"Save"}
-          customStyle={{
-            width: "100%",
-          }}
-        />
-      </Form.Item>
-    </div>
+    </Form>
   );
 }
 
