525a38dea327fd362fb91c532881df2787b8ac07
idara914 <idara914@gmail.com>
2025-04-22 14:21:08 -0700

    Update SignIn.tsx

--- VitalCare/src/screens/SignIn.tsx
+++ VitalCare/src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, StyleSheet, Text, View, Image, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
+import { StatusBar, StyleSheet, Text, View, Image, TouchableOpacity, KeyboardAvoidingView, Platform, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors, Icons } from "../constants/theme";
@@ -13,46 +13,43 @@ const SignIn = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <StatusBar translucent={true} backgroundColor={"transparent"} barStyle="dark-content" />
-      <KeyboardAvoidingView
-        style={styles.keyboardAvoidingView}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}
-      >
-        <View style={styles.container}>
-          <Image source={Icons.logo2} resizeMode="contain" style={styles.logo} />
+      <ScrollView>
+        <KeyboardAvoidingView style={styles.keyboardAvoidingView} behavior={Platform.OS === "ios" ? "padding" : "height"} keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}>
+          <View style={styles.container}>
+            <Image source={Icons.logo2} resizeMode="contain" style={styles.logo} />
 
-          <View style={styles.formWrapper}>
-            <View style={styles.formContainer}>
-              <Text style={styles.title}>Login</Text>
+            <View style={styles.formWrapper}>
+              <View style={styles.formContainer}>
+                <Text style={styles.title}>Login</Text>
 
-              <View style={styles.inputWrapper}>
-                <Text style={styles.inputLabel}>Email Address</Text>
-                <InputField placeholder="Enter your email address" />
-              </View>
+                <View style={styles.inputWrapper}>
+                  <Text style={styles.inputLabel}>Email Address</Text>
+                  <InputField placeholder="Enter your email address" />
+                </View>
 
-              <View style={styles.inputWrapper}>
-                <Text style={styles.inputLabel}>Password</Text>
-                <InputField placeholder="Enter your Password" password={true} />
+                <View style={styles.inputWrapper}>
+                  <Text style={styles.inputLabel}>Password</Text>
+                  <InputField placeholder="Enter your Password" password={true} />
+                </View>
               </View>
             </View>
           </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
+      <View style={styles.buttonWrapper}>
+        <CustomButton
+          title="Login"
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        />
+        <View style={styles.footerTextWrapper}>
+          <Text style={styles.footerText}>Don't have an account?</Text>
+          <TouchableOpacity activeOpacity={0.8} onPress={() => navigation.navigate("SignUp")}>
+            <Text style={styles.footerLink}>Register</Text>
+          </TouchableOpacity>
         </View>
-
-        <View style={styles.buttonWrapper}>
-          <CustomButton
-            title="Login"
-            onPress={() => {
-              navigation.navigate("Home");
-            }}
-          />
-          <View style={styles.footerTextWrapper}>
-            <Text style={styles.footerText}>Don't have an account?</Text>
-            <TouchableOpacity activeOpacity={0.8} onPress={() => navigation.navigate("SignUp")}>
-              <Text style={styles.footerLink}>Register</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </KeyboardAvoidingView>
+      </View>
     </SafeAreaView>
   );
 };
@@ -105,7 +102,8 @@ const styles = StyleSheet.create({
   buttonWrapper: {
     alignSelf: "center",
     width: "90%",
-    top: RFPercentage(40),
+    position: "absolute",
+    bottom: RFPercentage(4),
   },
   footerTextWrapper: {
     flexDirection: "row",
