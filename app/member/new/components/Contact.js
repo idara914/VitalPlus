520e2fdc66d480f5f3c5c673e6b807408215c997
idara914 <idara914@gmail.com>
2025-04-24 23:26:37 -0500

    Update Contact.js

--- app/member/new/components/Contact.js
+++ app/member/new/components/Contact.js
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { Form, Input } from "antd";
-import axios from "axios";
 import styles from "./Contact.module.css";
 import Button from "@/app/components/common/Button/Button";
 
@@ -10,80 +9,71 @@ const customStyle = {
   marginRight: "10px",
 };
 
-function Contact() {
-  const [form] = Form.useForm();
-
-  const handleSave = async () => {
-    const values = form.getFieldsValue(true);
-    try {
-      await axios.post("/api/save-patient", values);
-      alert("Saved successfully");
-    } catch (e) {
-      console.error("Failed to save", e);
-      alert("Failed to save");
-    }
-  };
-
+function Contact({ form, onClick }) {
   return (
-    <Form form={form}>
-      <div className={styles.mainContainer}>
-        <div className={styles.gridContainer}>
-          <Form.Item
-            label="Emergency Contact Name"
-            style={customStyle}
-            name="emergencyContactName"
-          >
-            <Input placeholder="Enter here" />
-          </Form.Item>
-          <Form.Item label="Relationship" name="relationship">
-            <Input placeholder="Enter here" />
-          </Form.Item>
-          <Form.Item label="Contact" name="contact" style={customStyle}>
-            <Input placeholder="Enter here" />
-          </Form.Item>
-          <Form.Item label="Address" name="address">
-            <Input placeholder="Enter here" />
-          </Form.Item>
+    <div className={styles.mainContainer}>
+      <div className={styles.gridContainer}>
+        <Form.Item
+          label="Emergency Contact Name"
+          style={customStyle}
+          name="emergencyContactName"
+        >
+          <Input placeholder="Enter here" />
+        </Form.Item>
 
-          <Form.Item
-            label="Secondary Physician"
-            name="secondaryPhysician"
-            style={customStyle}
-          >
-            <Input placeholder="Enter here" />
-          </Form.Item>
-          <Form.Item
-            label="Secondary Physician Number"
-            name="secondaryPhysicianNumber"
-          >
-            <Input placeholder="Enter here" />
-          </Form.Item>
+        <Form.Item label="Relationship" name="relationship">
+          <Input placeholder="Enter here" />
+        </Form.Item>
 
-          <Form.Item label="State" name="state" style={customStyle}>
-            <Input placeholder="Enter here" />
-          </Form.Item>
-          <Form.Item label="City" name="city">
-            <Input placeholder="Enter here" />
-          </Form.Item>
+        <Form.Item label="Contact" name="contact" style={customStyle}>
+          <Input placeholder="Enter here" />
+        </Form.Item>
 
-          <Form.Item label="Zip Code" name="zip" style={customStyle}>
-            <Input placeholder="Enter here" />
-          </Form.Item>
-        </div>
+        <Form.Item label="Address" name="address">
+          <Input placeholder="Enter here" />
+        </Form.Item>
 
-        <Form.Item>
-          <Button
-            htmltype="submit"
-            onClick={handleSave}
-            text={"Save"}
-            customStyle={{
-              width: "100%",
-            }}
-          />
+        <Form.Item
+          label="Secondary Physician"
+          name="secondaryPhysician"
+          style={customStyle}
+        >
+          <Input placeholder="Enter here" />
+        </Form.Item>
+
+        <Form.Item
+          label="Secondary Physician Number"
+          name="secondaryPhysicianNumber"
+        >
+          <Input placeholder="Enter here" />
+        </Form.Item>
+
+        <Form.Item label="State" name="state" style={customStyle}>
+          <Input placeholder="Enter here" />
+        </Form.Item>
+
+        <Form.Item label="City" name="city">
+          <Input placeholder="Enter here" />
+        </Form.Item>
+
+        <Form.Item label="Zip Code" name="zip" style={customStyle}>
+          <Input placeholder="Enter here" />
         </Form.Item>
       </div>
-    </Form>
+
+      <Form.Item>
+        <Button
+          htmltype="submit"
+          onClick={onClick}
+          text="Save"
+          customStyle={{
+            width: "100%",
+          }}
+        />
+      </Form.Item>
+    </div>
   );
 }
 
 export default Contact;
+
