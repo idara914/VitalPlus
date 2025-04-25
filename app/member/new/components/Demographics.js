78ee39c0a5936946d6652aa069ca0b680fccc615
idara914 <idara914@gmail.com>
2025-04-25 16:51:18 -0500

    Update Demographics.js

--- app/member/new/components/Demographics.js
+++ app/member/new/components/Demographics.js
@@ -1,5 +1,8 @@
+
+"use client";
+
 import React from "react";
-import { Checkbox, DatePicker, Form, Input, Radio } from "antd";
+import { DatePicker, Form, Input, Radio, Select } from "antd";
 import styles from "./Demographics.module.css";
 import Button from "@/app/components/common/Button/Button";
 
@@ -7,25 +10,107 @@ const customStyle = {
   marginRight: "10px",
 };
 
+const ethnicityOptions = [
+  { label: "Hispanic or Latino", value: "hispanic" },
+  { label: "Not Hispanic or Latino", value: "not_hispanic" },
+  { label: "Puerto Rican", value: "puerto_rican" },
+  { label: "Mexican", value: "mexican" },
+  { label: "Cuban", value: "cuban" },
+  { label: "Dominican", value: "dominican" },
+  { label: "Central American", value: "central_american" },
+  { label: "South American", value: "south_american" },
+  { label: "Other Hispanic or Latino", value: "other_hispanic" },
+  { label: "Decline to Answer", value: "decline" },
+];
+
+const raceOptions = [
+  {
+    label: "Asian",
+    options: [
+      { label: "Asian Indian", value: "asian_indian" },
+      { label: "Chinese", value: "chinese" },
+      { label: "Filipino", value: "filipino" },
+      { label: "Japanese", value: "japanese" },
+      { label: "Korean", value: "korean" },
+      { label: "Vietnamese", value: "vietnamese" },
+      { label: "Other Asian", value: "other_asian" },
+    ],
+  },
+  {
+    label: "Black or African American",
+    options: [
+      { label: "African American", value: "african_american" },
+      { label: "African (e.g. Nigerian, Ethiopian)", value: "african" },
+      { label: "Caribbean (e.g. Jamaican, Haitian)", value: "caribbean" },
+      { label: "Other Black", value: "other_black" },
+    ],
+  },
+  {
+    label: "White",
+    options: [
+      { label: "White (European descent)", value: "white" },
+      { label: "Middle Eastern (e.g. Arab, Persian)", value: "middle_eastern" },
+      { label: "North African (e.g. Egyptian, Moroccan)", value: "north_african" },
+      { label: "Other White", value: "other_white" },
+    ],
+  },
+  {
+    label: "American Indian or Alaska Native",
+    options: [
+      { label: "American Indian", value: "american_indian" },
+      { label: "Alaska Native", value: "alaska_native" },
+      { label: "Tribal Affiliation", value: "tribal_affiliation" },
+    ],
+  },
+  {
+    label: "Native Hawaiian or Other Pacific Islander",
+    options: [
+      { label: "Native Hawaiian", value: "native_hawaiian" },
+      { label: "Guamanian or Chamorro", value: "guamanian" },
+      { label: "Samoan", value: "samoan" },
+      { label: "Tongan", value: "tongan" },
+      { label: "Other Pacific Islander", value: "other_pacific_islander" },
+    ],
+  },
+  {
+    label: "Other",
+    options: [
+      { label: "Multiracial", value: "multiracial" },
+      { label: "Other", value: "other" },
+      { label: "Decline to Answer", value: "decline" },
+    ],
+  },
+];
+
 function DemographicForm({ onClick, form }) {
   return (
     <div className={styles.mainContainer}>
       <h1 className={styles.heading}>Ethnicity</h1>
 
-      <Form.Item name="ethicity">
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(2, 1fr)",
-            gap: "10px",
-            textAlign: "left",
-          }}
-        >
-          <Checkbox>No, not of Hispanic, Latino or Spanish</Checkbox>
-          <Checkbox>Yes, Mexican, Mexican American</Checkbox>
-          <Checkbox>Yes, Cuban</Checkbox>
-          <Checkbox>Other Hispanic, Latino or Spanish origin</Checkbox>
-        </div>
+      <Form.Item
+        label="Ethnicity"
+        name="ethnicity"
+        rules={[{ required: true, message: "Please select ethnicity!" }]}
+      >
+        <Select
+          options={ethnicityOptions}
+          placeholder="Select ethnicity"
+          style={{ width: "100%" }}
+        />
+      </Form.Item>
+
+      <h1 className={styles.heading}>Race</h1>
+
+      <Form.Item
+        label="Race"
+        name="race"
+        rules={[{ required: true, message: "Please select race!" }]}
+      >
+        <Select
+          options={raceOptions}
+          placeholder="Select race"
+          style={{ width: "100%" }}
+        />
       </Form.Item>
 
       <Form.Item
@@ -64,18 +149,6 @@ function DemographicForm({ onClick, form }) {
         />
       </Form.Item>
 
-      <h1 className={styles.heading}>Patient Preference</h1>
-      <Form.Item
-        name="patientPreference"
-        style={{ textAlign: "left" }}
-      >
-        <Radio.Group>
-          <Radio value="Family">Family</Radio>
-          <Radio value="Friend">Friend</Radio>
-          <Radio value="Professional">Professional</Radio>
-        </Radio.Group>
-      </Form.Item>
-
       <Button
         onClick={onClick}
         text="Save"
@@ -88,5 +161,3 @@ function DemographicForm({ onClick, form }) {
 }
 
 export default DemographicForm;
-
-
